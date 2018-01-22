800b2d1f07c977fb96572d0c7b1b395f07437c2b
oystein-macbook <haugseth@me.com>
2018-01-22 08:42:20 +0100

    adjust color

--- render.js
+++ render.js
@@ -3,13 +3,11 @@
 // jshint esversion: 6
 
 // set size of characters
-let matrixSize = 12;
-// hold array of streams
-let streams = [];
+let letterSize = 12;
+// hold array of streamArray
+let streamArray = [];
 // variable for graphik
 let graphik;
-// index character
-let index = 0;
 
 function preload() {
   graphik = loadFont('Graphik-Regular.otf');
@@ -21,16 +19,16 @@ function setup() {
   textFont(graphik);
   // x set horizontal start of matrix
   let x = 0;
-  // generate amount of streams
-  for (let i = 0; i <= (width / matrixSize); i++) {
+  // generate amount of streamArray
+  for (let i = 0; i <= (width / letterSize); i++) {
     let stream = new Stream();
     // randomize start parameter
-    stream.generateMatrices(x, random(-1000, 0));
-    streams.push(stream);
-    // horizontal space between streams
-    x += matrixSize + 10;
+    stream.generateStreamArray(x, random(-1000, 0));
+    streamArray.push(stream);
+    // horizontal space between streamArray
+    x += letterSize + 10;
   }
-  textSize(matrixSize);
+  textSize(letterSize);
 }
 
 function draw() {
@@ -38,7 +36,7 @@ function draw() {
   background(29, 29, 40, 200);
   // iterate over index character
   // calls the rendering
-  streams.forEach(function(stream) {
+  streamArray.forEach(function(stream) {
     stream.render();
   });
 }
@@ -49,54 +47,52 @@ function Matrix(x, y, speed, first) {
   this.x = x;
   this.y = y;
   this.speed = speed;
-  // refresh rate of new characters
+  // refresh rate of characters
   this.switchInterval = round(random(10, 20));
   this.first = first;
   this.index = 0;
-  // function to generate random characters
-  this.setToRandomMatrix = function() {
+  // function to load letters
+  this.setToStreamLetters = function() {
     if (frameCount % this.switchInterval == 0) {
-      let streamLetters = ['S', 'O', 'L', 'U', 'T', 'I', 'O', 'N', ' ', 'S', 'E', 'E', 'K', 'E', 'R', ' '];
-      // this.character = streamLetters;
+      let streamLetters = ['S', 'O', 'L', 'U', 'T', 'I', 'O', 'N', 'S', 'E', 'E', 'K', 'E', 'R', ' '];
+      // pull letters from array in sequence
       this.index += 1;
-      if (this.index > 15) {
+      if (this.index >= streamLetters.length) {
         this.index = 0;
       }
       this.character = streamLetters[this.index];
     }
   };
-  // function to change y position of characters
-  this.rain = function() {
-    // if y is equal to height it has reached bottom and is reset to 0, if not add speed
+  // function to loop y position of streams
+  this.yLimit = function() {
+    // if y is equal to height it has reached bottom and is reset to 0, if not keep momentum
     this.y = (this.y >= height) ? 0 : this.y += this.speed;
   };
 }
 
+
 function Stream() {
   // array of streams
-  this.Matrices = [];
+  this.streams = [];
   // length of streams
-  this.totalMatrices = round(random(5, 14));
+  this.streamsLength = round(random(5, 14));
   // speed (vertical movement)
   this.speed = random(1, 3);
-
-
-
-  this.generateMatrices = function(x, y) {
+  this.generateStreamArray = function(x, y) {
     // chance of light first character
     let first = round(random(0, 4)) === 1;
-    // function to render matrices
-    for (let i = 0; i <= this.totalMatrices; i++) {
+    // function to render streams
+    for (let i = 0; i <= this.streamsLength; i++) {
       matrix = new Matrix(x, y, this.speed, first);
-      matrix.setToRandomMatrix();
-      this.Matrices.push(matrix);
+      matrix.setToStreamLetters();
+      this.streams.push(matrix);
       // space between characters
-      y -= matrixSize + 14;
+      y -= letterSize + 14;
       first = false;
     }
   };
   this.render = function() {
-    this.Matrices.forEach(function(matrix) {
+    this.streams.forEach(function(matrix) {
       if (matrix.first) {
         // color for bright first color
         fill(166, 255, 198);
@@ -106,9 +102,9 @@ function Stream() {
       }
       // function to randomly display characters from this.character at x and y coordinates
       text(matrix.character, matrix.x, matrix.y);
-      // calls the rain function
-      matrix.rain();
-      matrix.setToRandomMatrix();
+      // calls the yLimit function
+      matrix.yLimit();
+      matrix.setToStreamLetters();
     });
   };
 }
